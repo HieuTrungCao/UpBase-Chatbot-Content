ee3e52129fcc3f57de213806fbebf51a77a72024
HieuTrungCao <21021490@vnu.edu.vn>
2025-02-21 11:26:27 +0700

    Add content list thread

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .resp_queue import SingletonQueue
 from .callback import call_llm
 from .utils import get_structure, get_style
+from .content import ContentList
---
+++ src/utils/content.py
@@ -0,0 +1,27 @@
+import threading
+
+from typing import List
+
+class ContentList:
+    __instance = None
+    __lock = threading.Lock()
+
+    def __init__(self):
+        if ContentList.__instance is not None:
+            raise Exception("Only sighle bot created")
+        
+        self.contents: List[str] = []
+
+    @staticmethod
+    def get_instance():
+        with ContentList.__lock:
+            if ContentList.__instance is None:
+                ContentList.__instance = ContentList()
+            
+            return ContentList.__instance
+        
+    def get_content(self) -> str:
+        return self.contents[-1]
+
+    def put_content(self, content):
+        self.contents.append(content)
